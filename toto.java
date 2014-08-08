e2c61f195297c90fb37151f444d91443023b8a2e
farous <fares.mokhtari@yahoo.fr>
2014-08-08 15:23:51 +0200

    Create toto.java

---
+++ toto.java
@@ -0,0 +1,8 @@
+/**
+ * Mojo utilisant svnkit et maven invoker pour effectuer une release à partir de la trunk
+ * 
+ * @goal release-trunk
+ * 
+ * @phase process-sources
+ */
+public class ReleaseMojo extends AbstractMojo {...}
